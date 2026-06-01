861de228409f213e8e5994e027a2e4a1598bf27f
santiago h <kunriejeke@gmail.com>
2026-06-01 18:53:36 -0500

    [bd] actualizacion de base

--- bd/ bd_multi.sql
+++ bd/ bd_multi.sql
@@ -2,15 +2,12 @@ create database bd_multi;
 use bd_multi;
 
 create table personaje (
-id_personaje int not null primary key,
+id_personaje int not null primary key auto increment,
 nombre varchar(20) not null,
 rol varchar(10) not null,
 nivel smallint not null,
 vida smallint not null , 
 daño smallint not null 
 );
-drop table personaje;
-select * from personaje;
-
 
 
